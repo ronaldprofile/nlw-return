e289303373b27e45b7f010971676c420d5d2e8f2
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-05-06 21:54:33 -0300

    backend tweaks for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { routes } from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen("4000", () => console.log("Server is running on PORT 4000"));
+app.listen(4000, () => console.log("Server is running on PORT 4000"));
